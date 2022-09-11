8f8e683a86ee10a721bb932fe4024469de31341e
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-11 14:53:01 +0530

    [roshan]maxumum number

---
+++ Day13AssigimentGeneric/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day13AssigimentGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" chooes number\n1. Find integer maximum number among 3 numbers");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    maxNNumber integerMaximumNumber = new maxNNumber();
+                    int intValue = integerMaximumNumber.PrintIntegerMaximumNumber(70, 85, 60);
+                    Console.WriteLine("Maximum number of integer is " + intValue);
+                    break;
+            }
+        }
+    }
+}
